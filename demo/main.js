b7a88005c278707157630777312045a692c3fe81
Sam Rose <samrose3@gmail.com>
2015-06-28 21:05:45 -0400

    -m Added editor toggle and data attributes to directive

--- demo/main.js
+++ demo/main.js
@@ -1,11 +1,28 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('demo-app', ['jv.json-viewer'])
-        .controller('MainCtrl', MainCtrl);
+  angular
+    .module('demo-app', ['jv.json-viewer'])
+    .controller('MainCtrl', MainCtrl);
 
-    function MainCtrl() {
+  MainCtrl.$inject = ['$http', 'jsonViewerService'];
 
-    }
+  function MainCtrl($http, jsonViewerService) {
+    var vm = this;
+    vm.showEditor = true;
+    vm.jsonData = {};
+
+    vm.toggleEditor = function() {
+      vm.showEditor = !vm.showEditor;
+    };
+
+    vm.useTestJson = function() {
+      var root = 'http://jsonplaceholder.typicode.com';
+
+      $http.get(root + '/posts/1/comments').then(function(result) {
+        jsonViewerService.jsonString = angular.toJson(result.data);
+        jsonViewerService.parseJson();
+      });
+    };
+  }
 })();
--- src/directives/jsonViewer.directive.js
+++ src/directives/jsonViewer.directive.js
@@ -23,9 +23,17 @@
     function linkFunc(scope, el, attr) { // jshint ignore:line
 
       scope.analyzeJson = function() {
-        jsonViewerService.jsonString = scope.editorText;
+        if(scope.editor)
+          jsonViewerService.jsonString = scope.editorText;
+        else if(scope.data) {
+          if(angular.isString(scope.data))
+            jsonViewerService.jsonString = scope.data;
+          else
+            jsonViewerService.jsonString = angular.toJson(scope.data, false);
+        }
         jsonViewerService.parseJson();
       };
+      
     }
 
   }
